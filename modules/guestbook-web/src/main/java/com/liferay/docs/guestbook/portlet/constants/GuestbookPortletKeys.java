1cdaf8dc141c2de1592c6a91e20bc08bc198b82d
Matej Dragicevic <matej.dragicevic@mercatis.com>
2018-06-07 12:22:02 +0200

    DaW: CaWP - Writing Your First Liferay Portal Application

---
+++ modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/constants/GuestbookPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.docs.guestbook.portlet.constants;
+
+/**
+ * @author matej
+ */
+public class GuestbookPortletKeys {
+
+	public static final String Guestbook = "Guestbook";
+
+}
